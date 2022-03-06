2d636b27d9ccc6bd00e4303786a8a50a3ed1d9ad
masspahmi <masspahmi@gmail.com>
2022-03-07 00:00:45 +0700

    handle error to json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Throwable;
+use Exception;
 
 class Handler extends ExceptionHandler
 {
@@ -26,7 +30,21 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+    protected function unauthenticated($request, \Illuminate\Auth\AuthenticationException $exception)
+    {
+        // Jika request via API
+        if ($request->expectsJson()) {
+            $response = new JsonResponse([
+                'success' => false,
+                'message' => 'unauthenticated',
+                'payload' => null,
+            ], 401);
 
+            return $response;
+        }
+
+        return redirect()->guest(route('main'));
+    }
     /**
      * Register the exception handling callbacks for the application.
      *
@@ -34,8 +52,42 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
+        $this->renderable(function (Exception $exception, $request) {
+            // dd($exception);
+            if ($exception instanceof NotFoundHttpException) {
+                if ($request->is('api/*')) {
+                    $response = new JsonResponse([
+                        'success' => false,
+                        'message' => 'Not found',
+                    ], 404);
+
+                    return $response;
+                }
+            }
+            if (method_exists($exception, "getStatusCode")) {
+                if ($exception->getStatusCode() == 404) {
+                    $response = new JsonResponse([
+                        'success' => false,
+                        'message' => 'Route not found',
+                    ], 404);
+
+                    return $response;
+                }
+            }
+        });
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        $response = new JsonResponse([
+            'success' => true,
+            'message' => 'Validation errors',
+            'payload' => $exception->errors(),
+        ], $exception->status);
+
+        return $response;
     }
 }
